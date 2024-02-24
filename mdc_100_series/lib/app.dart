b878ef850562fe89e748eced63517eec4baff259
athalie-aurora <119656945+athalie-aurora@users.noreply.github.com>
2024-02-24 07:07:11 +0700

    Change Color

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-
+import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
 
@@ -35,10 +35,49 @@ class ShrineApp extends StatelessWidget {
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
       // TODO: Customize the theme (103)
-      theme: ThemeData.light(useMaterial3: true),
+      theme: _buildShrineTheme(),
     );
   }
 }
 
 // TODO: Build a Shrine Theme (103)
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light(useMaterial3: true);
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrineBlue100,
+      onPrimary: kShrineIndigo900,
+      secondary: kShrineIndigo900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePink100,
+    ),
+  );
+}
+
 // TODO: Build a Shrine Text Theme (103)
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base.copyWith(
+    headlineSmall: base.headlineSmall!.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    titleLarge: base.titleLarge!.copyWith(
+      fontSize: 18.0,
+    ),
+    bodySmall: base.bodySmall!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    bodyLarge: base.bodyLarge!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: 'Rubik',
+    displayColor: kShrineBrown900,
+    bodyColor: kShrineBrown900,
+  );
+}
---
+++ mdc_100_series/lib/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//Pinky
+const kShrinePink50 = Color(0xFFFEEAE6);
+const kShrinePink100 = Color(0xFFFEDBD0);
+const kShrinePink300 = Color(0xFFFBB8AC);
+const kShrinePink400 = Color(0xFFEAA4A4);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
+
+// Blue
+const kShrineBlue50 = Color(0xFFE3F2FD);
+const kShrineBlue100 = Color(0xFFBBDEFB);
+const kShrineBlue300 = Color(0xFF64B5F6);
+const kShrineBlue400 = Color(0xFF42A5F5);
+
+const kShrineIndigo900 = Color(0xFF1A237E);
+
+
